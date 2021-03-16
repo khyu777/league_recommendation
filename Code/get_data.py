ecb7388a9e9c9bd0bc57a3eb16b6c323daf030b0
David Yu <khyu777@gmail.com>
2021-03-16 18:24:39 -0400

    minor changes to data pull to account for timeout

--- Code/get_data.py
+++ Code/get_data.py
@@ -12,57 +12,31 @@ api_key = os.environ.get("RIOT_API_KEY")
 total_time = dt.timedelta(0)
 total_calls = 0
 
-# Get all matches function
+# Get all matches
 def get_all_matches(region, id):
     now = dt.datetime.now()
     calls = 0
     global count, total_time, total_calls
-    try:
-        print(count, id)
-        match = watcher.match.matchlist_by_account(region, id, queue = 420)
-        calls += 1
-    except ApiError as err:
-        print(err.response.status_code)
-        if err.response.status_code == 429:
-            print('We should retry in {} seconds.'.format(err.headers['Retry-After']))
-            print('this retry-after is handled by default by the RiotWatcher library')
-            print('future requests wait until the retry-after time passes')
-        elif err.response.status_code == 404:
-            print('Summoner with that ridiculous name not found.')
-        elif err.response_status_code == 504:
-            time.sleep(10)
-        else:
-            raise
+    print(count, id)
+    match = watcher.match.matchlist_by_account(region, id, queue = 420)
+    calls += 1
     beginIndex = 100
     endIndex = 200
     time.sleep(10/12)
-    while endIndex <= 1000:
+    while endIndex <= 100:
         print(count, id, endIndex)
-        try:
-            match_addl = watcher.match.matchlist_by_account(region, id, queue = 420, begin_index = beginIndex)
-            match['matches'] = match['matches'] + match_addl['matches']
-            beginIndex += 100
-            endIndex += 100
-            time.sleep(10/12)
-            calls += 1
-        except ApiError as err:
-            print(err.response.status_code)
-            if err.response.status_code == 429:
-                print('We should retry in {} seconds.'.format(err.headers['Retry-After']))
-                print('this retry-after is handled by default by the RiotWatcher library')
-                print('future requests wait until the retry-after time passes')
-            elif err.response.status_code == 404:
-                print('Summoner with that ridiculous name not found.')
-            else:
-                raise
+        match_addl = watcher.match.matchlist_by_account(region, id, queue = 420, begin_index = beginIndex)
+        match['matches'] = match['matches'] + match_addl['matches']
+        beginIndex += 100
+        endIndex += 100
+        time.sleep(10/12)
+        calls += 1
     count += 1
     duration = dt.datetime.now() - now
     total_calls += calls
     total_time += duration
     print(duration, "Calls: {}".format(calls))
     print("Total time: {}".format(total_time), "Total calls: {}".format(total_calls))
-    if total_calls % 100 == 0:
-        time.sleep(60)
     return(match)
 
 # accountId print function
@@ -74,11 +48,31 @@ def acct_id_print(my_region, id):
     time.sleep(10/12)
     return(acct_id)
 
+# get match info
+def get_match_info(region, id):
+    now = dt.datetime.now()
+    calls = 0
+    global count, total_time, total_calls
+    print(count, id)
+    match = watcher.match.by_id(region, id)
+    calls += 1
+    beginIndex = 100
+    endIndex = 200
+    time.sleep(10/12)
+    count += 1
+    duration = dt.datetime.now() - now
+    total_calls += calls
+    total_time += duration
+    print(duration, "Calls: {}".format(calls))
+    print("Total time: {}".format(total_time), "Total calls: {}".format(total_calls))
+    return(match)
+
 #global vars
-watcher = LolWatcher(api_key)
+watcher = LolWatcher(api_key, timeout = 300)
 my_region = 'na1'
 
 master_df = pd.read_csv('Raw Data/queue_list.csv')
+master_df = master_df.iloc[1:6,]
 master_df.reset_index(inplace=True)
 
 print('Getting matches...')
@@ -86,7 +80,6 @@ count = 1
 master_df.loc[:, 'match'] = [get_all_matches(my_region, x) for x in master_df.acct_id]
 master_df = master_df.join(pd.json_normalize(master_df.match))
 print('Finished getting matches')
-print(master_df.dtypes, master_df)
 
 lens = [len(item) for item in master_df['matches']]
 master_df = pd.DataFrame(
@@ -107,12 +100,13 @@ champion_key['key'] = pd.to_numeric(champion_key['key'])
 
 master_df = pd.merge(master_df, champion_key, how = 'left', left_on = 'champion', right_on = 'key')
 print('Added champion names')
+print(master_df.shape)
 
 master_df.to_csv('Raw Data/master_solo_queue_all.csv', index = False)
 
 # Get Match Information
 print('Getting match information')
-match_info = pd.DataFrame([watcher.match.by_id(my_region, x) for x in master_df.head()['gameId']])[['gameId', 'teams', 'participants', 'participantIdentities']]
+match_info = pd.DataFrame([get_match_info(my_region, x) for x in master_df['gameId']])[['gameId', 'teams', 'participants', 'participantIdentities']]
 
 lens = [len(item) for item in match_info['teams']]
 match_info_teams = pd.DataFrame(
@@ -124,6 +118,7 @@ match_info_teams = match_info_teams.explode('bans').reset_index(drop = True)
 match_info_teams = match_info_teams.join(pd.json_normalize(match_info_teams.bans))
 match_info_teams.drop(columns = ['teams', 'bans'], inplace = True)
 print('Finished getting match information')
+print(match_info_teams.shape)
 
 lens = [len(item) for item in match_info['participants']]
 match_info_pts = pd.DataFrame(
@@ -138,6 +133,7 @@ pt_ids.drop(columns = ['participants', 'participantIdentities'], inplace = True)
 print('Finished getting participants')
 
 pt_match_all = pts.merge(pt_ids, on = ['participantId', 'gameId'])
+print(pt_match_all.shape)
 
 match_info_teams.to_csv('Raw Data/match_team_info_all.csv', index = False)
 pt_match_all.to_csv('Raw Data/match_pt_info_all.csv', index = False)
